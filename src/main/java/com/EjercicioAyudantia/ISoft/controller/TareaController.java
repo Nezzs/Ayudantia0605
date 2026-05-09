07f6aaf5b38e06732a30e0c7c4d5e565202ccdd1
Guillermo <g.salgado04@ufromail.cl>
2026-05-09 05:33:14 -0400

    implementacion de endpoint get para filtrado de tareas.

--- src/main/java/com/EjercicioAyudantia/ISoft/controller/TareaController.java
+++ src/main/java/com/EjercicioAyudantia/ISoft/controller/TareaController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.EjercicioAyudantia.ISoft.dto.RequestTareaDTO;
 import com.EjercicioAyudantia.ISoft.model.Tarea;
 import com.EjercicioAyudantia.ISoft.service.PostTareaService;
+import com.EjercicioAyudantia.ISoft.service.GetTareaFilterService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
 
 
 
@@ -17,15 +22,26 @@ import com.EjercicioAyudantia.ISoft.service.PostTareaService;
 public class TareaController {
 
     private final PostTareaService postTareaService;
+    private final GetTareaFilterService getTareaFilterService;
 
-    public TareaController(PostTareaService postTareaService) {
+    public TareaController(PostTareaService postTareaService, GetTareaFilterService getTareaFilterService) {
         this.postTareaService = postTareaService;
+        this.getTareaFilterService = getTareaFilterService;
     }
 
     @PostMapping()
     public ResponseEntity<Tarea> createTask(@RequestBody RequestTareaDTO requestTareaDTO) {
         return ResponseEntity.created(null).body(postTareaService.createTask(requestTareaDTO));
     }
-    
+
+    @GetMapping()
+    public ResponseEntity<List<Tarea>> getFilteredTasks(
+        @RequestParam(required = false) String prioridad,
+        @RequestParam(required = false) String fechaLimite,
+        @RequestParam(required = false) String titulo) {
+            
+            return ResponseEntity.ok(getTareaFilterService.getFilteredTasks(prioridad, fechaLimite, titulo));
+    }
 }
+
     
